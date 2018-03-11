8edb9f1d6b130e15c37e3af1891a1333b02cc566
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-03-11 17:10:58 +0100

    Add basic REST Server.
    Based on the spring-boot rest tutorial: https://spring.io/guides/gs/rest-service/

---
+++ src/main/java/at/ac/uibk/keyless/GreetingController.java
@@ -0,0 +1,19 @@
+package at.ac.uibk.keyless;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+  private static final String template = "Hello, %s! I bims 1 Server.";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Greeting(counter.incrementAndGet(),
+      String.format(template, name));
+  }
+}
