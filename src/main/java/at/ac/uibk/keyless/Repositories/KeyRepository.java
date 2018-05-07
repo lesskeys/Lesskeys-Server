46664c2a5816824fc612c8fc2c213ab16a1d26a9
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-05-07 15:35:52 +0200

    Add controller and service for key.

--- src/main/java/at/ac/uibk/keyless/Repositories/KeyRepository.java
+++ src/main/java/at/ac/uibk/keyless/Repositories/KeyRepository.java
@@ -2,10 +2,12 @@ package at.ac.uibk.keyless.Repositories;
 
 import at.ac.uibk.keyless.Models.Key;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Lukas Dötlinger.
  */
+@Repository
 public interface KeyRepository extends JpaRepository<Key, Long> {
 
   Key findByKeyId(Long keyId);
--- src/main/java/at/ac/uibk/keyless/Repositories/LockRepository.java
+++ src/main/java/at/ac/uibk/keyless/Repositories/LockRepository.java
@@ -2,10 +2,12 @@ package at.ac.uibk.keyless.Repositories;
 
 import at.ac.uibk.keyless.Models.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Lukas Dötlinger.
  */
+@Repository
 public interface LockRepository extends JpaRepository<Lock, Long> {
 
   Lock findByLockId(Long lockId);
