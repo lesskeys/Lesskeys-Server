74fdfdaa880f410ed56fdc61c3ec1a89f275f112
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-08-20 11:30:06 +0200

    Log a warning if firebase could not be initialized.

--- src/main/java/at/ac/uibk/keyless/FirebaseConfig.java
+++ src/main/java/at/ac/uibk/keyless/FirebaseConfig.java
@@ -3,6 +3,8 @@ package at.ac.uibk.keyless;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
@@ -18,16 +20,22 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+
   @PostConstruct
-  public void init() throws IOException {
-    FileInputStream serviceAccount = new FileInputStream(
-      ResourceUtils.getFile("classpath:firebase-adminsdk.json"));
+  public void init() {
+    try {
+      FileInputStream serviceAccount = new FileInputStream(
+        ResourceUtils.getFile("classpath:firebase-adminsdk.json"));
 
-    FirebaseOptions options = new FirebaseOptions.Builder()
-      .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-      .setDatabaseUrl("https://keyless-app.firebaseio.com")
-      .build();
+      FirebaseOptions options = new FirebaseOptions.Builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://keyless-app.firebaseio.com")
+        .build();
 
-    FirebaseApp.initializeApp(options);
+      FirebaseApp.initializeApp(options);
+    } catch (IOException e) {
+      log.warn("FirebaseApp could not be initialized. Check if the firebase-adminsdk.json exists in resources.");
+    }
   }
 }
