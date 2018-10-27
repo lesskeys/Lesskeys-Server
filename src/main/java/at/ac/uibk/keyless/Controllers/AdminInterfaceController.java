464a9da0737ee9ba2059df84961572efad2c51e2
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-10-27 13:32:02 +0200

    Add login functionality for admin interface.

--- src/main/java/at/ac/uibk/keyless/Controllers/AdminInterfaceController.java
+++ src/main/java/at/ac/uibk/keyless/Controllers/AdminInterfaceController.java
@@ -1,11 +1,15 @@
 package at.ac.uibk.keyless.Controllers;
 
 import at.ac.uibk.keyless.Models.Lock;
+import at.ac.uibk.keyless.Models.User;
 import at.ac.uibk.keyless.Services.LockService;
+import at.ac.uibk.keyless.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Lukas Dötlinger.
@@ -16,6 +20,21 @@ public class AdminInterfaceController {
   @Autowired
   LockService lockService;
 
+  @Autowired
+  UserService userService;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+
+  @RequestMapping(value = "/ai/login", method = RequestMethod.POST)
+  public boolean adminLogin(@RequestBody Map<String, String> data) {
+    String username = data.get("username");
+    User user = userService.getUserByEmail(username);
+    String password = data.get("password");
+    return (userService.hasRole(user, "Admin") &&
+      passwordEncoder.matches(password, user.getPassword()));
+  }
 
   @RequestMapping(value = "/ai/locks", method = RequestMethod.GET)
   public List<Lock> getAllLocks() {
