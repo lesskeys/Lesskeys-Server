708b5d4c860f6ec3e260571aabbdd4b1602d0615
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-11-13 22:23:33 +0100

    Add RingMessage model and repository.

--- src/main/java/at/ac/uibk/keyless/Repositories/RingMessageRepository.java
+++ src/main/java/at/ac/uibk/keyless/Repositories/RingMessageRepository.java
@@ -1,6 +1,6 @@
 package at.ac.uibk.keyless.Repositories;
 
-import at.ac.uibk.keyless.Models.Session;
+import at.ac.uibk.keyless.Models.RingMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Repository;
  * Created by Lukas Dötlinger.
  */
 @Repository
-public interface SessionRepository extends JpaRepository<Session, Long> {
+public interface RingMessageRepository extends JpaRepository<RingMessage, Long> {
 
-  Session findBySessionId(Long sessionId);
-
-  Session findByUserId(Long userId);
+  RingMessage findByMessageId(Long messageId);
 }
